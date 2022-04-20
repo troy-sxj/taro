f34398cab8478789bb0ee306ebfc4175b26b2a0f
shangxiaojian <shangacademy@hotmail.com>
2022-04-20 10:17:59 +0800

    feat(harmony): taro api添加apiSdkVersion

--- packages/taro-harmony/src/apis/index.ts
+++ packages/taro-harmony/src/apis/index.ts
@@ -1,9 +1,11 @@
 import { noop, current } from './utils'
 import * as apis from './apis'
+const deviceInfo = require('@ohos.deviceInfo')
 
 export function initNativeApi (taro) {
   current.taro = taro
   taro.initPxTransform = noop
+  taro.apiSdkVersion = deviceInfo.sdkApiVersion
   Object.defineProperty(taro, 'getApp', {
     configurable: true,
     enumerable: true,
